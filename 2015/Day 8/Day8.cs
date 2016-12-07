c4e79e271e5a29d72ee9e93f4dbaab29d5e625ad
Tobias Wallenqvist <tobias@wallenqvist.se>
2016-12-07 09:32:36 +0100

    day 8 finished

---
+++ 2015/Day 8/Day8.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode {
+	
+	class Day8 {
+		
+		public static void Main(string[] args) {
+			
+			string[] instructions = System.IO.File.ReadAllLines(@"../../input.txt");
+			
+			Step1(instructions);
+			Step2(instructions);
+			
+			Console.ReadKey(true);
+		}
+		
+		public static void Step1(string[] instructions) {
+			
+			int sumLineChars = 0;
+			int sumUnescapedChars = 0;
+
+			foreach (string line in instructions) {
+	
+				sumLineChars += line.Length;
+				sumUnescapedChars += Regex.Unescape(line).Length;
+                    
+				if(line.StartsWith("\"")) {
+					sumUnescapedChars--;
+				}
+    
+				if(line.EndsWith("\"")) {
+					sumUnescapedChars--;
+				}
+			}    
+			
+			Console.WriteLine("Answer Part 1 : " + (sumLineChars - sumUnescapedChars));
+		}
+		
+		public static void Step2(string[] instructions) {
+
+			int sumEscapedChars = 0;
+			int sumLineChars = 0;
+			
+			foreach (string line in instructions) {
+				sumLineChars += line.Length;
+
+				sumEscapedChars +=2;
+				
+				for(int i = 0; i < line.Length; i++) {
+					if (line[i] == '\\') {
+						sumEscapedChars++;
+					}
+					
+					if (line[i] == '"') {
+						sumEscapedChars++;
+					}
+					
+					sumEscapedChars++;
+				}
+			}    
+			
+			Console.WriteLine("Answer Part 2 : " + (sumEscapedChars - sumLineChars));
+		}
+	}
+}
