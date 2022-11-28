2db6bb7cc5e0633c6ea6ee6773825302d0a0e06b
Tobias Wallenqvist <tobias@wallenqvist.se>
2022-11-28 19:40:22 +0100

    Day 1 upload

---
+++ 2020/Day 01/Day1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Drawing;
+
+namespace AdventOfCode {
+	class Day1 {
+		
+		
+		public static void Main(string[] args) {
+        	
+			string[] instructions = System.IO.File.ReadAllLines(@"../../input.txt");
+				
+			Step1(instructions[0]);
+			Step2(instructions[0]);
+        	
+			Console.ReadKey(true);
+		}
+
+		public static void Step1(string instructions) {
+			
+			
+			
+			Console.WriteLine("Answer Part 1 : " + xxxx);
+		}
+		
+		public static void Step2(string instructions) {
+			
+			
+			
+			Console.WriteLine("Answer Part 2 : " + xxxx);
+		}
+	}
+}
---
+++ 2021/Day 01/readme.md
@@ -0,0 +1,25 @@
+## Header
+
+Text
+
+####For example,
+```
+Example text
+```
+
+This is the question
+
+and my answer was 181.
+
+###--- Part Two ---
+
+text part 2.
+
+####For example, 
+```
+Exaple text 2
+```
+
+This is the second question
+
+and my answer was 140.
