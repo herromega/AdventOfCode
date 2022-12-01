6866dce78bc13162cb57f97532ad8504098c281d
Tobias Wallenqvist <tobias@wallenqvist.se>
2022-12-01 19:41:47 +0100

    Day 01 2022

--- 2022/Day 01/Day1.cs
+++ 2022/Day 01/Day1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Drawing;
+using System.Collections.Immutable;
 
 namespace AdventOfCode.Y2022 {
 	class Day1 {
@@ -10,26 +11,59 @@ namespace AdventOfCode.Y2022 {
         public static string xxxx = "";
         public static void Main(string[] args) {
         	
-			string[] instructions = System.IO.File.ReadAllLines(@"../../input.txt");
+			string[] instructions = System.IO.File.ReadAllLines(@"../../../Day 01/input.txt");
 				
-			Step1(instructions[0]);
-			Step2(instructions[0]);
+			Step1(instructions);
+			Step2(instructions);
         	
 			Console.ReadKey(true);
 		}
 
-		public static void Step1(string instructions) {
-			
-			
-			
-			Console.WriteLine("Answer Part 1 : " + xxxx);
+		public static void Step1(string[] instructions) {
+
+			int elfNumber = 1;
+            List<int> elfCarrying = new List<int>();
+
+			int elfCalories = 0;
+
+            foreach (string logRow in instructions) {
+				if(!int.TryParse(logRow, out int logRowValue)) {
+
+					elfCarrying.Add(elfCalories);
+					elfCalories = 0;
+                    elfNumber++;
+				}
+
+				elfCalories += logRowValue;
+            }
+
+            Console.WriteLine("Answer Part 1 : " + elfCarrying.Max().ToString());
 		}
 		
-		public static void Step2(string instructions) {
-			
-			
-			
-			Console.WriteLine("Answer Part 2 : " + xxxx);
+		public static void Step2(string[] instructions) {
+
+            int elfNumber = 1;
+            List<int> elfCarrying = new List<int>();
+
+            int elfCalories = 0;
+
+            foreach (string logRow in instructions)
+            {
+                if (!int.TryParse(logRow, out int logRowValue))
+                {
+
+                    elfCarrying.Add(elfCalories);
+                    elfCalories = 0;
+                    elfNumber++;
+                }
+
+                elfCalories += logRowValue;
+            }
+
+            elfCarrying.Sort();
+            elfCarrying.Reverse();
+
+            Console.WriteLine("Answer Part 2 : " + elfCarrying.Take(3).Sum().ToString());
 		}
 	}
 }
