42d8e1293536dcee0ed45adac3c6fbc97303b7b7
Tobias Wallenqvist <tobias@wallenqvist.se>
2022-12-04 15:14:31 +0100

    Day 01 2018

--- 2018/Day 01/Day1.cs
+++ 2018/Day 01/Day1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Drawing;
+using System.Xml.Linq;
 
 namespace AdventOfCode.Y2018 {
     class Day1 {
@@ -10,26 +11,51 @@ namespace AdventOfCode.Y2018 {
         public static string xxxx = "";
         public static void Main(string[] args) {
         	
-			string[] instructions = System.IO.File.ReadAllLines(@"../../input.txt");
-				
-			Step1(instructions[0]);
-			Step2(instructions[0]);
+			string[] instructions = System.IO.File.ReadAllLines(@"../../../Day 01/input.txt");
+
+            Step1(instructions);
+			Step2(instructions);
         	
 			Console.ReadKey(true);
 		}
 
-		public static void Step1(string instructions) {
-			
-			
-			
-			Console.WriteLine("Answer Part 1 : " + xxxx);
+		public static void Step1(string[] instructions) {
+
+			int currentFrequency = 0;
+
+			foreach (string frequencyChange in instructions) {
+                currentFrequency += int.Parse(frequencyChange);
+            }
+
+            Console.WriteLine("Answer Part 1 : " + currentFrequency);
 		}
 		
-		public static void Step2(string instructions) {
-			
-			
-			
-			Console.WriteLine("Answer Part 2 : " + xxxx);
+		public static void Step2(string[] instructions) {
+
+            int currentFrequency = 0;
+            bool foundFrequencyTwice = false;
+
+            List<int> frequencyList = new List<int>();
+
+            string frequencyChange = "";
+
+            for (int i = 0; i < instructions.Length; i++) {
+
+                frequencyChange = instructions[i];
+                currentFrequency += int.Parse(frequencyChange);
+
+                if (frequencyList.Contains(currentFrequency)) {
+                    foundFrequencyTwice = true;
+                    break;
+                } else {
+                    frequencyList.Add(currentFrequency);
+                }
+
+                if (i == (instructions.Length - 1) && !foundFrequencyTwice) { i = -1; }
+            }
+
+
+            Console.WriteLine("Answer Part 2 : " + currentFrequency);
 		}
 	}
 }
