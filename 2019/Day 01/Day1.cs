0a87ec679c43d7199b4116096ec728933ba6f1d0
Tobias Wallenqvist <tobias@wallenqvist.se>
2022-12-04 14:27:37 +0100

    Day 01 2019

--- 2019/Day 01/Day1.cs
+++ 2019/Day 01/Day1.cs
@@ -9,27 +9,52 @@ namespace AdventOfCode.Y2019 {
 
         public static string xxxx = "";
         public static void Main(string[] args) {
-        	
-			string[] instructions = System.IO.File.ReadAllLines(@"../../input.txt");
-				
-			Step1(instructions[0]);
-			Step2(instructions[0]);
+
+            string[] instructions = System.IO.File.ReadAllLines(@"../../../Day 01/input.txt");
+
+            Step1(instructions);
+			Step2(instructions);
         	
 			Console.ReadKey(true);
 		}
 
-		public static void Step1(string instructions) {
-			
-			
-			
-			Console.WriteLine("Answer Part 1 : " + xxxx);
+		public static void Step1(string[] instructions) {
+
+			double fuelReqiurementsPerModule = 0;
+			double fuelRequirementInTotal = 0;
+
+			foreach (string moduleMass in instructions) {
+
+				fuelReqiurementsPerModule = (Math.Floor(double.Parse(moduleMass) / 3) - 2);
+
+                fuelRequirementInTotal += fuelReqiurementsPerModule;
+            }
+
+            Console.WriteLine("Answer Part 1 : " + fuelRequirementInTotal);
 		}
 		
-		public static void Step2(string instructions) {
-			
-			
-			
-			Console.WriteLine("Answer Part 2 : " + xxxx);
+		public static void Step2(string[] instructions) {
+
+            double fuelReqiurementsPerModule = 0;
+            double fuelAdditionalRequirementsPerModule = 0;
+            double fuelRequirementInTotal = 0;
+
+            foreach (string moduleMass in instructions) {
+
+                fuelReqiurementsPerModule = (Math.Floor(double.Parse(moduleMass) / 3) - 2);
+                fuelAdditionalRequirementsPerModule = fuelReqiurementsPerModule;
+
+
+                while (fuelAdditionalRequirementsPerModule > 0) {
+                    fuelAdditionalRequirementsPerModule = (Math.Floor(fuelAdditionalRequirementsPerModule / 3) - 2);
+
+                    if (fuelAdditionalRequirementsPerModule >= 0 ) {
+                        fuelReqiurementsPerModule += fuelAdditionalRequirementsPerModule;
+                    }
+                }
+
+                fuelRequirementInTotal += fuelReqiurementsPerModule;
+            }
 		}
 	}
 }
