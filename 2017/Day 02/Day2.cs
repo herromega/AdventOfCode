73da342e69e7f8148300a9a5437c4fd59037d186
Tobias Wallenqvist <tobias@wallenqvist.se>
2022-12-04 20:21:13 +0100

    Day 02 2017

--- 2017/Day 02/Day2.cs
+++ 2017/Day 02/Day2.cs
@@ -9,27 +9,49 @@ namespace AdventOfCode.Y2017 {
 
         public static string xxxx = "";
         public static void Main(string[] args) {
-        	
-			string[] instructions = System.IO.File.ReadAllLines(@"../../input.txt");
-				
-			Step1(instructions[0]);
-			Step2(instructions[0]);
+
+            string[] instructions = System.IO.File.ReadAllLines(@"../../../Day 02/input.txt");
+
+            Step1(instructions);
+			Step2(instructions);
         	
 			Console.ReadKey(true);
 		}
 
-		public static void Step1(string instructions) {
-			
-			
-			
-			Console.WriteLine("Answer Part 1 : " + xxxx);
+		public static void Step1(string[] instructions) {
+
+            int spreadsheetCellSum = 0;
+
+            foreach (string spreadsheetRow in instructions) {
+				int[] spreadSheetCells = spreadsheetRow.Split('\t').Select(int.Parse).OrderByDescending(x => x).ToArray();
+
+                spreadsheetCellSum += (spreadSheetCells.First() - spreadSheetCells.Last());
+            }
+
+            Console.WriteLine("Answer Part 1 : " + spreadsheetCellSum);
 		}
 		
-		public static void Step2(string instructions) {
-			
-			
-			
-			Console.WriteLine("Answer Part 2 : " + xxxx);
+		public static void Step2(string[] instructions) {
+
+            int spreadsheetCellSum = 0;
+
+            foreach (string spreadsheetRow in instructions) {
+                int[] spreadSheetCells = spreadsheetRow.Split('\t').Select(int.Parse).OrderByDescending(x => x).ToArray();
+
+                for(int i = 0; i < spreadSheetCells.Length; i++) {
+                    for (int j = 0; j < spreadSheetCells.Length; j++) {
+
+                        if (i == j) { continue; }
+
+                        if (spreadSheetCells[i] % spreadSheetCells[j] == 0) {
+                            spreadsheetCellSum += (spreadSheetCells[i] / spreadSheetCells[j]);
+                        }
+                    }
+                }
+
+            }
+
+            Console.WriteLine("Answer Part 2 : " + spreadsheetCellSum);
 		}
 	}
 }
