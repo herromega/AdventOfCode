1774b8c89c56022971504e5511da39ac0da7cb65
Tobias Wallenqvist <tobias@wallenqvist.se>
2022-12-06 19:36:03 +0100

    Day 06 2022

---
+++ 2022/Day 06/Day6.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Drawing;
+
+namespace AdventOfCode.Y2022 {
+	class Day6 {
+
+        public static string xxxx = "";
+        public static void Main(string[] args) {
+        	
+			string instructions = System.IO.File.ReadAllText(@"../../../Day 06/input.txt");
+				
+			Step1(instructions);
+			Step2(instructions);
+        	
+			Console.ReadKey(true);
+		}
+
+		public static void Step1(string instructions) {
+
+			int StartOfPacketMarkerFoundAt = 0;
+
+            for (int packetMarkerCounter = 3; packetMarkerCounter < instructions.Length; packetMarkerCounter++) {
+
+                string packetMarkerTestBatch = instructions.Substring(packetMarkerCounter - 3, 4);
+
+				if(packetMarkerTestBatch.Distinct().Count() == 4) {
+                    StartOfPacketMarkerFoundAt = packetMarkerCounter + 1;
+					break;
+                }
+            }
+
+            Console.WriteLine("Answer Part 1 : " + StartOfPacketMarkerFoundAt);
+		}
+		
+		public static void Step2(string instructions) {
+
+            int StartOfPacketMarkerFoundAt = 0;
+
+            for (int packetMarkerCounter = 13; packetMarkerCounter < instructions.Length; packetMarkerCounter++) {
+
+                string packetMarkerTestBatch = instructions.Substring(packetMarkerCounter - 13, 14);
+
+                if (packetMarkerTestBatch.Distinct().Count() == 14)
+                {
+                    StartOfPacketMarkerFoundAt = packetMarkerCounter + 1;
+                    break;
+                }
+            }
+
+            Console.WriteLine("Answer Part 2 : " + StartOfPacketMarkerFoundAt);
+		}
+	}
+}
